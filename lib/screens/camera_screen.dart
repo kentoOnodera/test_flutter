d9aec59c04e1bd77f2f5e9fcc3fd593a821f4afa
kento-onodera <kento.onodera@lasfactory.com>
2023-09-09 12:23:18 +0900

    カメラのページのみ作成

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -1,4 +1,21 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  // main 関数内で非同期処理を呼び出すための設定
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // デバイスで使用可能なカメラのリストを取得
+  final cameras = await availableCameras();
+
+  // 利用可能なカメラのリストから特定のカメラを取得
+  final firstCamera = cameras.first;
+
+  // 取得できているか確認
+  print(firstCamera);
+
+  runApp(const CameraScreen());
+}
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({
